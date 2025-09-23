3e83d27876b029279f079660a07e0e7a8ad41074
monicaardila <monicayisethardilagudelo@gmail.com>
2025-09-23 07:50:00 -0500

    config local

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend_genuine/', // Nombre correcto de tu repositorio
+  base: process.env.NODE_ENV === 'production' ? '/frontend_genuine/' : '/',
   define: {
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://127.0.0.1:8000/api')
   }
