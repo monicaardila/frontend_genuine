97b7b4703d03a7c2336728800b41c592ac753159
ing_monicaardila <monica.ardila@cofincafe.com>
2025-09-23 10:22:22 -0500

    modificacion vite

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-//export default defineConfig({
-plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/frontend_genuine/' : '/',
-    define: {
-  'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://127.0.0.1:8000/api')
-}
-//})
 export default defineConfig({
   plugins: [react()],
-  base: './',   // 👈 usar rutas relativas
+  base: './',
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://127.0.0.1:8000/api')
+    'import.meta.env.VITE_API_URL': JSON.stringify(
+      process.env.VITE_API_URL || 'http://127.0.0.1:8000/api'
+    )
   }
 })
